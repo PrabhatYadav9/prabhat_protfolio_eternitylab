069dc5dc9274d96ec4a292152bb0670ca9d7aa31
PrabhatYadav9 <prabhaty701@gmail.com>
2026-02-10 03:46:29 +0530

    project img

--- script.js
+++ script.js
@@ -5,4 +5,26 @@ cvButton.addEventListener("click", () => {
     alert("CV download feature coming soon!");
 });
 
+// hamburger 
+const ham = document.querySelector(".hamburger");
+const menu = document.querySelector("nav ul");
+
+ham.onclick = () => {
+    menu.classList.toggle("active");
+};
+
+// scroll reveal
+const sections = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+    sections.forEach(sec => {
+        if (sec.getBoundingClientRect().top < window.innerHeight - 50) {
+            sec.classList.add("show");
+        }
+    });
+});
+
+
+
+
 
